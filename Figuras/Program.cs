7082e14550588846295cbd654e7ea487c22f3aae
JUAN PABLO UPEGUI ORTIZ <upegui2007@hotmail.com>
2023-08-30 21:32:30 -0500

    triangulo

---
+++ Figuras/Program.cs
@@ -0,0 +1,30 @@
+﻿// Dados los catetos de un triángulo rectángulo, calcular su hipotenusa
+public class Triangulo
+{
+    static void Main(string[] arg)
+    {
+        //datos de entrada
+        int Cateto1, Cateto2, Hipotenusa;
+        Console.WriteLine("Ingresa el valor del cateto 1");
+        Cateto1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingresa el valor del cateto 2");
+        Cateto2 = Convert.ToInt32(Console.ReadLine());
+            
+        //Proceso
+        Hipotenusa = Cateto1*Cateto1 + Cateto2*Cateto2;
+
+        //Salida
+        Console.WriteLine("El Valor de la Hipotenusa es: "+ Hipotenusa);
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
